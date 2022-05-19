12074789380d309850bffa9890759b6b96c598a5
Jasbox <jasonxu092@gmail.com>
2022-05-19 03:11:42 +0100

    view individual article

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Article from './components/Article/Article';
 import ArticleList from './components/Article/ArticleList';
 import Header from './components/Header/Header';
 
@@ -12,6 +13,7 @@ function App() {
       <Route path='/' element={<ArticleList />} />
       <Route path='/topics' element={<ArticleList />} />
       <Route path='/topics/:topic' element={<ArticleList />} />
+      <Route path='/articles/:article_id' element={<Article />} />
     </Routes>
     
     </div>
