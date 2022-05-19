7473a854beac94383c4240cc0305b886d184311b
Jasbox <jasonxu092@gmail.com>
2022-05-19 11:44:46 +0100

    user can delete own comments

--- src/components/Comment.jsx/Comments.jsx
+++ src/components/Comment.jsx/Comments.jsx
@@ -1,15 +1,29 @@
 
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { fetchComments } from "../../api";
+import { UserContext } from "../User/UserContext";
+import { deleteComment } from "../../api";
 
 export default function Comments({ article_id }) {
   const [comments, setComments] = useState([]);
+  const {users} = useContext(UserContext)
 
   useEffect(() => {
     fetchComments(article_id).then((comments) => {
       setComments(comments);
     });
   }, [article_id]);
+
+  function checkAuthor(author, comment_id) {
+    if (author === users) {
+        return(
+            <button onClick={()=> {
+                deleteComment(comment_id)
+                alert("comment deleted")
+            }}>Delete</button>
+        )
+    }
+  }
 
   return (
     <div>
@@ -19,6 +33,7 @@ export default function Comments({ article_id }) {
           <div className="comments" key={comment.comment_id}>
             {`by ${comment.author} at ${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`}
             <p>{comment.body}</p>
+            {checkAuthor(comment.author, comment.comment_id)}
           </div>
         );
       })}
